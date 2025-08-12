0ee13e726648f1d793f3dd43bbd7c05c3b11fd3f
Emma10003 <5emma10003@gmail.com>
2025-08-12 18:16:02 +0900

    index 페이지 Section 1 텍스트 애니메이션 효과

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 $(function () {
+    /* ------------------------------
+    Section 1 : 텍스트 애니메이션
+    ------------------------------ */
+    // 텍스트 쪼개기
     deepDataSplitLetters();
+    // 이펙트 클래스 추가
+    const effectChar = $(".charChild");
+    effectChar.addClass("intro-effect");
+    
+    // 애니메이션 끝난 후 클래스 제거
+    effectChar.on("animationend", function () {
+        $(this).removeClass("intro-effect");
+    });
 });
 
 /* ===========================================================
@@ -10,13 +22,13 @@ Section 1
 // 텍스트 쪼개기
 function deepDataSplitLetters() {
     for (let i = 0; i < 2; i++) {
-        let content = $(`#text-row${i+1}`).text();
+        let content = $(`#text-row${i + 1}`).text();
         console.log("content: ", content);
         let html = content
             .split("")
             .map((char) => `<span class="charChild">${char}</span>`)
             .join("");
 
-        $(`#text-row${i+1}`).html(html);
+        $(`#text-row${i + 1}`).html(html);
     }
 }
