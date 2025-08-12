88c58e72bd73af35f5f1c59bda824dcd09dd7506
Emma10003 <5emma10003@gmail.com>
2025-08-12 16:45:42 +0900

    index 네비게이션, 글자 쪼개서 span태그로 감싸는 함수

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-$(function(){
+$(function () {
+    deepDataSplitLetters();
+});
 
-})
+/* ===========================================================
+Section 1
+=========================================================== */
+// 글자 하나하나 big->normal 사이즈로 날아오는 것 처럼 보이는 효과
 
-// Section 1: 글자 하나하나 big->normal 사이즈로 날아오는 것 처럼 보이는 효과
+// 텍스트 쪼개기
+function deepDataSplitLetters() {
+    for (let i = 0; i < 2; i++) {
+        let content = $(`#text-row${i+1}`).text();
+        console.log("content: ", content);
+        let html = content
+            .split("")
+            .map((char) => `<span class="charChild">${char}</span>`)
+            .join("");
+
+        $(`#text-row${i+1}`).html(html);
+    }
+}
